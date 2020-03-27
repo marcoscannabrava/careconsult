5586ab1b0ace96bb2e5314de4161b0bd33ad8388
Marcos Cannabrava <mpcannabrava@gmail.com>
2020-03-27 10:09:46 -0300

    implement feat: authentication

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,11 @@
-import React, { Component, Fragment }  from 'react'
-import { Helmet } from 'react-helmet'
-import './all.sass'
-import useSiteMetadata from './SiteMetadata'
-import { withPrefix } from 'gatsby'
+import React, { Component, Fragment }  from 'react';  
 
-import Navigation from './Navigation';
 import getFirebase, { FirebaseContext } from './Firebase';
 import withAuthentication from './Session/withAuthentication';
 
-import Navbar from './Navbar'
-import Footer from './Footer'
+import TemplateWrapper from './LayoutTemplate';
 
-class LayoutTemplate extends Component {
+class Layout extends Component {
   state = {
     firebase: null,
   };
@@ -37,54 +31,13 @@ class LayoutTemplate extends Component {
   }
 }
 
-const { title, description } = useSiteMetadata()
 const AppWithAuthentication = withAuthentication(({ children }) => (
   <Fragment>
     {/* <Navigation /> */}
-    <div>
-      <Helmet>
-        <html lang="en" />
-        <title>{title}</title>
-        <meta name="description" content={description} />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/stethoscope-icon.png`}//32x32 is preferred
-          sizes="32x32"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix('/')}img/stethoscope-icon.png`}//16x16 is preferred
-          sizes="16x16"
-        />
-
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
-        <meta name="theme-color" content="#fff" />
-
-        <meta property="og:type" content="business.business" />
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
-        />
-      </Helmet>
-      <Navbar />
-      <div>{children}</div>
-      <Footer />
-    </div>
+    <TemplateWrapper>
+      {children}
+    </TemplateWrapper>
   </Fragment>
 ));
 
-export default LayoutTemplate;
+export default Layout;
--- src/components/LayoutTemplate.js
+++ src/components/LayoutTemplate.js
@@ -1,115 +1,61 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import './all.sass'
+import useSiteMetadata from './SiteMetadata'
+import { withPrefix } from 'gatsby'
 
-import Navigation from './Navigation';
-import getFirebase, { FirebaseContext } from './Firebase';
-import withAuthentication from './Session/withAuthentication';
+const TemplateWrapper = ({ children }) => {
+  const { title, description } = useSiteMetadata()
+  return (
+    <div>
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
 
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          sizes="16x16"
+        />
 
-class LayoutTemplate extends Component {
-  state = {
-    firebase: null,
-  };
+        <link
+          rel="mask-icon"
+          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+          color="#ff4400"
+        />
+        <meta name="theme-color" content="#fff" />
 
-  componentDidMount() {
-    const app = import('firebase/app');
-    const auth = import('firebase/auth');
-    const database = import('firebase/database');
+        <meta property="og:type" content="business.business" />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content="/" />
+        <meta
+          property="og:image"
+          content={`${withPrefix('/')}img/og-image.jpg`}
+        />
+      </Helmet>
+      <Navbar />
+      <div>{children}</div>
+      <Footer />
+      <script src="https://www.gstatic.com/firebasejs/7.13.0/firebase-app.js"></script>
+      <script src="https://www.gstatic.com/firebasejs/7.13.0/firebase-analytics.js"></script>
 
-    Promise.all([app, auth, database]).then(values => {
-      const firebase = getFirebase(values[0]);
-
-      this.setState({ firebase });
-    });
-  }
-
-  render() {
-    return (
-      <FirebaseContext.Provider value={this.state.firebase}>
-        <AppWithAuthentication {...this.props} />
-      </FirebaseContext.Provider>
-    );
-  }
+    </div>
+  )
 }
 
-const AppWithAuthentication = withAuthentication(({ children }) => (
-  <Fragment>
-    <Navigation />
-    <hr />
-    { children }
-  </Fragment>
-));
-
-// export default LayoutTemplate;
-
-
-
-
-
-
-
-
-
-
-
-
-// Layout.JS
-
-// import React from 'react'
-// import { Helmet } from 'react-helmet'
-// import './all.sass'
-// import useSiteMetadata from './SiteMetadata'
-// import { withPrefix } from 'gatsby'
-
-// import Navbar from './Navbar'
-// import Footer from './Footer'
-
-// const TemplateWrapper = ({ children }) => {
-//   const { title, description } = useSiteMetadata()
-//   return (
-//     <div>
-//       <Helmet>
-//         <html lang="en" />
-//         <title>{title}</title>
-//         <meta name="description" content={description} />
-
-//         <link
-//           rel="apple-touch-icon"
-//           sizes="180x180"
-//           href={`${withPrefix('/')}img/apple-touch-icon.png`}
-//         />
-//         <link
-//           rel="icon"
-//           type="image/png"
-//           href={`${withPrefix('/')}img/stethoscope-icon.png`}//32x32 is preferred
-//           sizes="32x32"
-//         />
-//         <link
-//           rel="icon"
-//           type="image/png"
-//           href={`${withPrefix('/')}img/stethoscope-icon.png`}//16x16 is preferred
-//           sizes="16x16"
-//         />
-
-//         <link
-//           rel="mask-icon"
-//           href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-//           color="#ff4400"
-//         />
-//         <meta name="theme-color" content="#fff" />
-
-//         <meta property="og:type" content="business.business" />
-//         <meta property="og:title" content={title} />
-//         <meta property="og:url" content="/" />
-//         <meta
-//           property="og:image"
-//           content={`${withPrefix('/')}img/og-image.jpg`}
-//         />
-//       </Helmet>
-//       <Navbar />
-//       <div>{children}</div>
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default TemplateWrapper
+export default TemplateWrapper
