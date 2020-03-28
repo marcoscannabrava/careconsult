cff2a1c7a98b421292594ef650bcebc417b478f8
Marcos Cannabrava <mpcannabrava@gmail.com>
2020-03-28 18:02:17 -0300

    add feature: schedule available time slots, pending user authentication as provider

---
+++ firebase-functions/functions/provider/show_availability.js
@@ -0,0 +1,13 @@
+// Import Admin SDK
+const admin = require("firebase-admin");
+
+// Get a database reference to our posts
+const db = admin.database();
+const ref = db.ref("server/saving-data/fireblog/posts");
+
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on("value", function(snapshot) {
+  console.log(snapshot.val());
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
